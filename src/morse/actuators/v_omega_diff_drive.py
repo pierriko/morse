851615afa8b268908661605c4b9d73dfc5da58dd
David Hodo <david.hodo@gmail.com>
2011-07-25 23:39:53 -0500

    working on segway diff drive model

---
+++ src/morse/actuators/v_omega_diff_drive.py
@@ -0,0 +1,62 @@
+import GameLogic
+import morse.core.actuator
+
+class VWDiffDriveActuatorClass(morse.core.actuator.MorseActuatorClass):
+    """ Motion controller using linear and angular speeds
+
+    This class will read linear and angular speeds (V, W)
+    as input from an external middleware, and then apply them
+    to the parent robot.  Differs from the standard V,W controller
+    in that individual wheel speeds are controlled rather than 
+    chassis speed
+    """
+
+    def __init__(self, obj, parent=None):
+        print ('######## VW CONTROL INITIALIZATION ########')
+        # Call the constructor of the parent class
+        super(self.__class__,self).__init__(obj, parent)
+
+        self.local_data['v'] = 0.0
+        self.local_data['w'] = 0.0
+
+        # get track width for calculating wheel speeds from yaw rate
+        parent = self.robot_parent
+        self.trackWidth = parent.local_data['trackWidth']
+        self.radius = parent.local_data['wheelRadius']
+
+        print ('######## CONTROL INITIALIZED ########')
+
+
+
+    def default_action(self):
+        """ Apply (v, w) to the parent robot. """
+
+        # calculate desired wheel speeds and set them
+
+        if (self.local_data['v']<0.01)and(self.local_data['w']<0.01):
+            #self.robot_parent.blender_obj.setLinearVelocity([0.0, 0.0, 0.0], True)
+            #self.robot_parent.blender_obj.setAngularVelocity([0.0, 0.0, 0.0], True)
+            # set wheel speeds - front and rear wheels have the same speed
+            #self.robot_parent.local_data['wheelFL'].setAngularVelocity([0.0,0.0,-0.1],True)
+            #self.robot_parent.local_data['wheelFR'].setAngularVelocity([0.0,0.0,-0.1],True)
+            #self.robot_parent.local_data['wheelRL'].setAngularVelocity([0.0,0.0,0.1],True)
+            #self.robot_parent.local_data['wheelRR'].setAngularVelocity([0.0,0.0,0.1],True)
+            #print('stopping')
+            pass
+        else:
+            # left and right wheel speeds in m/s
+            v_ws_l=(2*self.local_data['v']-self.local_data['w']*self.trackWidth)/2
+            v_ws_r=(2*self.local_data['v']+self.local_data['w']*self.trackWidth)/2
+
+            # convert to angular speeds
+            w_ws_l=v_ws_l/self.radius
+            w_ws_r=v_ws_r/self.radius
+            # set wheel speeds - front and rear wheels have the same speed
+            self.robot_parent.local_data['wheelFL'].setAngularVelocity([0.0,0.0,w_ws_l],True)
+            self.robot_parent.local_data['wheelFR'].setAngularVelocity([0.0,0.0,w_ws_r],True)
+            self.robot_parent.local_data['wheelRL'].setAngularVelocity([0.0,0.0,w_ws_l],True)
+            self.robot_parent.local_data['wheelRR'].setAngularVelocity([0.0,0.0,w_ws_r],True)
+        #print(w_ws_l,w_ws_r,self.local_data['v'],self.local_data['w'])
+
+
+
--- src/morse/robots/segwayrmp400.py
+++ src/morse/robots/segwayrmp400.py
@@ -21,18 +21,49 @@ class SegwayRMP400Class(morse.core.robot.MorseRobotClass):
         #print(dir(scene.objects))
         #print(dir(scene.objects['rmp_wheel_atv.000']))
 
-        wheel1=scene.objects['rmp_wheel_atv.000']
-        wheel2=scene.objects['rmp_wheel_atv.001']
-        wheel3=scene.objects['rmp_wheel_atv.002']
-        wheel4=scene.objects['rmp_wheel_atv.003']
+        self.local_data['wheelFL']=scene.objects['rmp_wheel_atv.000']
+        self.local_data['wheelFR']=scene.objects['rmp_wheel_atv.001']
+        self.local_data['wheelRL']=scene.objects['rmp_wheel_atv.002']
+        self.local_data['wheelRR']=scene.objects['rmp_wheel_atv.003']
+        self.local_data['numWheels']=4
+        
+        # get wheel radius
+        self.local_data['wheelRadius']=0.27  # TODO read this later from GameObjectSettings.radius
+       
+        print(self.local_data['wheelRR'].parent)
+        print(self.local_data['wheelRR'].getPhysicsId())
+        
+        
+        # get track width
+        posL=self.local_data['wheelRL'].position
+        posR=self.local_data['wheelRR'].position
+        # subtract y coordinates of wheels to get width
+        self.local_data['trackWidth']=posL[1]-posR[1]
+        print(self.local_data['trackWidth'])
 
-        wheel1.setAngularVelocity([0.0,0.0,5.0],True)
-        wheel2.setAngularVelocity([0.0,0.0,-5.0],True)
-        wheel3.setAngularVelocity([0.0,0.0,5.0],True)
-        wheel4.setAngularVelocity([0.0,0.0,-5.0],True)
+        # true parameters tell it velocities are local
+        self.local_data['wheelFL'].setAngularVelocity([0.0,0.0,0.0],True)
+        self.local_data['wheelFR'].setAngularVelocity([0.0,0.0,0.0],True)
+        self.local_data['wheelRL'].setAngularVelocity([0.0,0.0,0.0],True)
+        self.local_data['wheelRR'].setAngularVelocity([0.0,0.0,0.0],True)
 
         print ('######## ROBOT INITIALIZED ########')
+
+    def getWheelSpeeds(self):
+        # true parameters tell it velocities are local
+        # wheels should be rotating about local Z axis
+        wsFL=self.local_data['wheelFL'].getAngularVelocity(True)
+        wsFR=self.local_data['wheelFR'].getAngularVelocity(True)
+        wsRL=self.local_data['wheelRL'].getAngularVelocity(True)
+        wsRR=self.local_data['wheelRR'].getAngularVelocity(True)
+        return [wsFL[2], wsFR[2], wsRL[2], wsRR[2]]
+        
 
     def default_action(self):
         """ Main function of this component. """
         pass
+        
+        #self.local_data['wheelFL'].setAngularVelocity([0.0,0.0,2.0],True)
+        #self.local_data['wheelFR'].setAngularVelocity([0.0,0.0,-2.0],True)
+        #self.local_data['wheelRL'].setAngularVelocity([0.0,0.0,2.0],True)
+        #self.local_data['wheelRR'].setAngularVelocity([0.0,0.0,-2.0],True)
