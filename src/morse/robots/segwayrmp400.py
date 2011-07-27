14b251d75948b465e808688f8bcb8e2e558512c2
David Hodo <david.hodo@gmail.com>
2011-07-26 20:13:44 -0500

    cleaning up segwayrmp model

--- src/morse/robots/segwayrmp400.py
+++ src/morse/robots/segwayrmp400.py
@@ -3,7 +3,7 @@ import morse.core.robot
 #import bge.constraints
 #import bge.logic
 import PhysicsConstraints
-import bge
+import bpy
 import math
 
 class SegwayRMP400Class(morse.core.robot.MorseRobotClass):
@@ -19,20 +19,18 @@ class SegwayRMP400Class(morse.core.robot.MorseRobotClass):
         print ("######## ROBOT '%s' INITIALIZING ########" % obj.name)
         super(self.__class__,self).__init__(obj, parent)
         
+        # get a link to the blender scene to look for wheel and suspension objects
         scene = GameLogic.getCurrentScene()
-        #print(dir(scene))
-        #print(dir(scene.objects))
-        #print(dir(scene.objects['rmp_wheel_atv.000']))
-
+        
         # determines if vehicle has suspension or just wheels
-        self.hasSuspension=False
+        self.hasSuspension=self.blender_obj['HasSuspension']
 
         # get pointers to and physicsIds of all objects
         # get wheel pointers - needed by wheel speed sensors
-        self.local_data['wheelFL']=scene.objects['rmp_wheel_atv.000']
-        self.local_data['wheelFR']=scene.objects['rmp_wheel_atv.001']
-        self.local_data['wheelRL']=scene.objects['rmp_wheel_atv.002']
-        self.local_data['wheelRR']=scene.objects['rmp_wheel_atv.003']
+        self.local_data['wheelFL']=scene.objects[self.blender_obj['WheelFLName']]
+        self.local_data['wheelFR']=scene.objects[self.blender_obj['WheelFRName']]
+        self.local_data['wheelRL']=scene.objects[self.blender_obj['WheelRLName']]
+        self.local_data['wheelRR']=scene.objects[self.blender_obj['WheelRRName']]
         self.local_data['numWheels']=4
          # wheel ID's
         self.local_data['wheelFL_ID'] = self.local_data['wheelFL'].getPhysicsId()
@@ -42,8 +40,6 @@ class SegwayRMP400Class(morse.core.robot.MorseRobotClass):
         # body ID
         chassis_ID = self.blender_obj.getPhysicsId()
         # suspension arm ID's
-        
-        
         
         # set up wheel constraints
         # add wheels to either suspension arms or vehicle chassis
@@ -57,30 +53,14 @@ class SegwayRMP400Class(morse.core.robot.MorseRobotClass):
             self.local_data['wheelRLJoint']=self.AttachWheelToBody(self.local_data['wheelRL'],self.blender_obj) 
             self.local_data['wheelRRJoint']=self.AttachWheelToBody(self.local_data['wheelRR'],self.blender_obj) 
         
-        # try to lock a wheel
-        #wheelFLJoint.setParam(3,0.0,0.0) # no rotation about Y axis
-        
-        # unlock wheel
-        #wheelFLJoint.setParam(3,-10000.0,10000.0) # no rotation about Y axis
-        
-        #wheelFLJoint.setParam(9,0.0,10000.0)
-        #wheelFRJoint.setParam(9,0.0,10000.0)
-        #wheelRLJoint.setParam(9,0.0,10000.0)
-        #wheelRRJoint.setParam(9,0.0,10000.0)
-        
         # get wheel radius
         self.local_data['wheelRadius']=0.27  # TODO read this later from GameObjectSettings.radius
-       
-        print(self.local_data['wheelRR'].parent)
-        print(self.local_data['wheelRR'].getPhysicsId())
-        
-        
+
         # get track width
         posL=self.local_data['wheelRL'].position
         posR=self.local_data['wheelRR'].position
         # subtract y coordinates of wheels to get width
         self.local_data['trackWidth']=posL[1]-posR[1]
-        print(self.local_data['trackWidth'])
 
         print ('######## ROBOT INITIALIZED ########')
 
@@ -97,28 +77,14 @@ class SegwayRMP400Class(morse.core.robot.MorseRobotClass):
     def default_action(self):
         """ Main function of this component. """
         pass
-        
-        #self.local_data['wheelFL'].setAngularVelocity([0.0,0.0,2.0],True)
-        #self.local_data['wheelFR'].setAngularVelocity([0.0,0.0,-2.0],True)
-        #self.local_data['wheelRL'].setAngularVelocity([0.0,0.0,2.0],True)
-        #self.local_data['wheelRR'].setAngularVelocity([0.0,0.0,-2.0],True)
-        
-        
-    # TODO: these functions should be put in a separate module later!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-    
-    def GetRelativePos (self, obj_c,obj_p):
-        # could i do this better by just subtracting positions?
-        obj_c.setParent(obj_p)
-        pos = obj_c.localPosition
-        obj_c.removeParent()
-        return pos    
+
+    # TODO: these functions should be put in a separate module later or
+    # a new robot class created
         
     def AttachWheelToBody(self, wheel, parent):
         #wheelPos=self.GetRelativePos(wheel, parent)
         result = parent.getVectTo(wheel);
         wheelPos=result[0]*result[2]
-        print(result)
-        print(wheelPos)
         joint = PhysicsConstraints.createConstraint( parent.getPhysicsId(),
                                      wheel.getPhysicsId(),
                                      12,    # 6dof constraint
