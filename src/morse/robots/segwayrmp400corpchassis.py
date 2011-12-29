2043cba01f53244dc44e40feac13383d4e6f6edb
David Hodo <david.hodo@gmail.com>
2011-12-29 12:07:54 -0600

    corrected tire locations for new segway model

--- src/morse/robots/segwayrmp400corpchassis.py
+++ src/morse/robots/segwayrmp400corpchassis.py
@@ -43,7 +43,6 @@ class SegwayRMP400CorpChassisVehicleClass(morse.core.robot.MorseVehicleRobotClas
         """ Main function of this component. """
         pass
 
-
 class SegwayRMP400CorpChassisPhysicsClass(morse.core.robot.MorsePhysicsRobotClass):
     """ Class definition for the Segway RMP400 base.
         Sub class of Morse_Object. """
@@ -56,10 +55,10 @@ class SegwayRMP400CorpChassisPhysicsClass(morse.core.robot.MorsePhysicsRobotClas
         # Call the constructor of the parent class
         print ("######## ROBOT '%s' INITIALIZING ########" % obj.name)
         # define the wheel positions:
-        self.wheelFLPos=mathutils.Vector((-0.305, 0.286, 0.270))
-        self.wheelFRPos=mathutils.Vector((0.305, 0.286, 0.270))
-        self.wheelRLPos=mathutils.Vector((-0.305, -0.286, 0.270))
-        self.wheelRRPos=mathutils.Vector((0.305, -0.286, 0.270))
+        self.wheelFLPos=mathutils.Vector((.286, .305, -0.270))
+        self.wheelFRPos=mathutils.Vector((.286, -.305, -0.270))
+        self.wheelRLPos=mathutils.Vector((-.286, .305, -0.270))
+        self.wheelRRPos=mathutils.Vector((-.286, -.305, -0.270))
         
         super(self.__class__,self).__init__(obj, parent)
 
