8a82a662a0a8c97e2c367943452d4f952adf433f
David Hodo <david.hodo@gmail.com>
2012-06-25 14:00:27 -0500

    change for compatibility with ROS fuerte

--- src/morse/middleware/ros/tfMessage.py
+++ src/morse/middleware/ros/tfMessage.py
@@ -4,6 +4,7 @@ import struct
 
 import geometry_msgs.msg
 import std_msgs.msg
+import genpy
 
 class tfMessage(roslib.message.Message):
   _md5sum = "94810edda583a504dfda3829e70d7eec"
@@ -266,7 +267,7 @@ float64 w
     except struct.error as e:
       raise roslib.message.DeserializationError(e) #most likely buffer underfill
 
-_struct_I = roslib.message.struct_I
+_struct_I = genpy.struct_I
 _struct_4d = struct.Struct("<4d")
 _struct_2I = struct.Struct("<2I")
 _struct_3d = struct.Struct("<3d")
